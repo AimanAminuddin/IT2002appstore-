53949b97e108d0b71cd6c3078265a6a9ad09987f
Aiman <64759433+AimanAminuddin@users.noreply.github.com>
2022-03-15 16:21:37 +0800

    Add files via upload

---
+++ sql/AppClean.sql
@@ -0,0 +1,11 @@
+/*******************
+  Cleaning script
+*******************/
+
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS places;
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries; 
+DROP TABLE IF EXISTS hosts; 
+DROP TABLE IF EXISTS users; 
