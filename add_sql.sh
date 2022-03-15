cf28d19a53db4043004aeadaebd6a6ad9f4506f9
Aiman <64759433+AimanAminuddin@users.noreply.github.com>
2022-03-15 17:32:04 +0800

    Update add_sql.sh

--- add_sql.sh
+++ add_sql.sh
@@ -28,8 +28,18 @@ a = 1
 URI="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
 # Run the scripts to insert data.
-psql ${URI} -f sql/AppStoreClean.sql
-psql ${URI} -f sql/AppStoreSchema.sql
-psql ${URI} -f sql/AppStoreCustomers.sql
-psql ${URI} -f sql/AppStoreGames.sql
-psql ${URI} -f sql/AppStoreDownloads.sql
+#psql ${URI} -f sql/AppStoreClean.sql
+#psql ${URI} -f sql/AppStoreSchema.sql
+#psql ${URI} -f sql/AppStoreCustomers.sql
+#psql ${URI} -f sql/AppStoreGames.sql
+#psql ${URI} -f sql/AppStoreDownloads.sql
+
+psql ${URI} -f sql/AppClean.sql
+psql ${URI} -f sql/AirBnbSchema.sql
+psql ${URI} -f sql/AppUsers.sql
+psql ${URI} -f sql/AppHosts.sql
+psql ${URI} -f sql/AppCountries.sql
+psql ${URI} -f sql/AppCities.sql
+psql ${URI} -f sql/AppPlaces.sql
+psql ${URI} -f sql/Appbookings.sql
+psql ${URI} -f sql/AppReviews.sql
