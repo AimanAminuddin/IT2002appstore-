b4370add8e1de62d43e01810a6dac39448b2aae6
Aiman <64759433+AimanAminuddin@users.noreply.github.com>
2022-03-15 09:42:20 +0800

    Add files via upload

---
+++ sql/AppCountries.sql
@@ -0,0 +1,5 @@
+INSERT INTO countries VALUES('Australia');
+INSERT INTO countries VALUES('United States');
+INSERT INTO countries VALUES('United Kingdom');
+INSERT INTO countries VALUES('Germany');
+INSERT INTO countries VALUES('New Zealand');
