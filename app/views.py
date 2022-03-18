45c0ac0a6502191353ed00abb12742ce692fccd6
Ze Jian <zejiantanwork@gmail.com>
2022-03-18 22:13:16 +0800

    updated view.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
