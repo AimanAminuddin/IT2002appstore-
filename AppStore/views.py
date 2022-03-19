cf843f65b664ca626fef0c9b2148fa82a0889b95
Ze Jian <zejiantanwork@gmail.com>
2022-03-19 16:08:43 +0800

    commit

--- AppStore/views.py
+++ AppStore/views.py
@@ -100,20 +100,22 @@ def edit(request, id):
 
 
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    #username = request.POST.get('username')
-    #password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        auth_login(request, user)
-        # Redirect to a success page.
-        # request.session["username"] = username
-        
-        return redirect('mainpage')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #username = request.POST.get('username')
+        #password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            # Redirect to a success page.
+            # request.session["username"] = username
+            
+            return redirect('mainpage')
 
-        # return redirect('mainpage') <-- this is from what i saw on the youtube video
-    
+            # return redirect('mainpage') <-- this is from what i saw on the youtube video
+        else:
+            return redirect('login')
     else:
         # Return an 'invalid login' error message.
          return render(request, "login.html")
