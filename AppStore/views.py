5b5f6f7ad302ff500b406417b180b0406fb95d1e
Aiman Aminuddin <e0322692@u.nus.edu>
2022-03-20 10:17:12 +0800

    try if works

--- AppStore/views.py
+++ AppStore/views.py
@@ -101,7 +101,7 @@ def edit(request, id):
 
 
 
-
+'''''
 def login_view(request):
     if request.method == "POST":
         username = request.POST.get('username')
@@ -124,6 +124,25 @@ def login_view(request):
     else:
         # Return an 'invalid login' error message.
          return render(request, "login.html")
+'''
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				auth_login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})
 
 
 def logout(request):
