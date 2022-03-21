c600ad2f4c28c0fb3ac2cfd217eac3f14c75be5e
Aiman Aminuddin <e0322692@u.nus.edu>
2022-03-21 18:43:04 +0800

    Users table

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Users(models.Model):
+    user_id = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 50)
