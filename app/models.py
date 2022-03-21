760ec816dd826abccab89812a19464a51d741c33
Aiman Aminuddin <e0322692@u.nus.edu>
2022-03-21 18:45:21 +0800

    return user_id

--- app/models.py
+++ app/models.py
@@ -5,3 +5,6 @@ class Users(models.Model):
     user_id = models.CharField(max_length = 50)
     email = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.user_id
