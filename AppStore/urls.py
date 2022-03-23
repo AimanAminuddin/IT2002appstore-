2e1329207bfc6ccf8d074633b2c4bd69479dd54e
Aiman Aminuddin <e0322692@u.nus.edu>
2022-03-23 22:10:28 +0800

    place_view to place_index

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path("mainpage",views.index,name = "mainpage"),
     path('edit/<str:id>',views.edit, name='edit'),
     path('add',views.add,name = 'add'),
-    path('places',views.place_view,name = 'place_view')
+    path('places',views.place_index,name = 'place_view')
 ]
