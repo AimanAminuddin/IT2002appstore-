a3dbbe118d5709a4ce0d47040b75d9c83186ce1b
Aiman Aminuddin <e0322692@u.nus.edu>
2022-03-31 16:53:13 +0800

    fddsfdsfdf

--- AppStore/views.py
+++ AppStore/views.py
@@ -329,9 +329,8 @@ def place_booking(request):
                 # insert new booking into booking table 
                 # create booking id 
                 cursor.execute("SELECT booking_id FROM bookings ORDER BY booking_id DESC LIMIT = 1")
-                booking_id = cursor.fetchone() + 1 
-                print(booking_id)
-                temp = int(booking_id) + 1 
+                booking_id = cursor.fetchone() 
+                temp = str(int(booking_id) + 1) 
                 cursor.execute("INSERT INTO users bookings (%s, %s, %s,%s,%s)",[temp,username,address,start_date,end_date])
                 return HttpResponseRedirect(reverse("place"))
             
